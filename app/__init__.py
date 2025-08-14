de62b484b04a95970ed8db47ec0eb943d6f76cf6
Danny <a.ovalle@alumno.um.edu.ar>
2025-08-14 18:47:55 -0300

    structure updated

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+import logging
+from flask import Flask
+import os
+from app.config import config
+
+def create_app() -> Flask:
+    """
+    Using an Application Factory
+    Ref: Book Flask Web Development Page 78
+    """
+    app_context = os.getenv('FLASK_CONTEXT')
+    #https://flask.palletsprojects.com/en/3.0.x/api/#flask.Flask
+    app = Flask(__name__)
+    f = config.factory(app_context if app_context else 'development')
+    app.config.from_object(f)
+    
+    @app.shell_context_processor    
+    def ctx():
+        return {"app": app}
+    
+    return app
