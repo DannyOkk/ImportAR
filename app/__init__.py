feaa91c7d83f920574ed16fe9b8ee4902b3bf295
Danny <a.ovalle@alumno.um.edu.ar>
2025-08-21 19:40:11 -0300

    #10 Refactor database configuration and add connection tests for testing environment

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ import logging
 from flask import Flask
 import os
 from app.config import config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app() -> Flask:
     """
@@ -13,6 +16,7 @@ def create_app() -> Flask:
     app = Flask(__name__)
     f = config.factory(app_context if app_context else 'development')
     app.config.from_object(f)
+    db.init_app(app)
     
     @app.shell_context_processor    
     def ctx():
