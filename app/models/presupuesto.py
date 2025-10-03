fe2e7190cc79b8219016aa93e5e7ad8ca42e90a6
vani <va.fuentes@alumno.um.edu.ar>
2025-10-03 16:44:51 -0300

    #17 Refactor Presupuesto model to inherit from db.Model; define table structure and relationships

--- app/models/presupuesto.py
+++ app/models/presupuesto.py
@@ -1,10 +1,15 @@
 from dataclasses import dataclass
+from app import db
 from app.models.simulacion import Simulacion
 
 @dataclass(init=False)
-class Presupuesto:
-  simulacion: Simulacion
-  estado: str
-  moneda: str
-  total: float
-  detalle: str
+class Presupuesto(db.Model):
+  
+  __tablename__ = 'presupuestos'
+  id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  simulacion_id: int = db.Column(db.Integer, db.ForeignKey('simulaciones.id'), nullable=False)
+  simulacion = db.relationship('Simulacion', backref='presupuestos')
+  estado: str = db.Column(db.String(50), nullable=False)
+  moneda: str = db.Column(db.String(10), nullable=False)
+  total: float = db.Column(db.Float, nullable=False)
+  detalle: str = db.Column(db.Text, nullable=True)
