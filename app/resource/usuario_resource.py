be4ef53952eaca502c2e382bb2b3779de5aba658
Luna Romero <romeroluna000@gmail.com>
2025-10-03 17:31:44 -0300

    #23 Implement user creation and update endpoints; add validation decorator for request data

--- app/resource/usuario_resource.py
+++ app/resource/usuario_resource.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+from app.validators.validators import validate_with
+from flask import Blueprint, jsonify, request
 from app.mapping import UsuarioSchema, ResponseSchema
 from app.service import UsuarioService, ResponseBuilder
 from app.models.usuario import Usuario
@@ -23,3 +24,29 @@ def get_usuario(id: int):
         return message_mapper.dump(message, many=False), 404
     message=rb.add_message("Usuario encontrado").add_status_code(100).add_data(usuario_mapper.dump(usuario)).add_path(f"/api/v1/usuarios/{id}").build()
     return message_mapper.dump(message, many=False), 200
+
+
+@usuario_bp.route('/', methods=['POST'])
+@validate_with(UsuarioSchema)
+def create_usuario():
+    usuario = usuario_mapper.load(request.json)
+    UsuarioService.create(usuario)
+    rb= ResponseBuilder()
+    message=rb.add_message("Usuario creado en el sistema").add_status_code(201).add_data({}).add_path(f"/api/v1/usuarios/").build()
+        
+    return message_mapper.dump(message, many=False), 201
+
+@usuario_bp.route('/<int:id>', methods=['PUT'])
+def update_usuario(id: int):
+    usuario_data = usuario_mapper.load(request.json)
+    usuario = UsuarioService.update(usuario_data, id)
+    rb= ResponseBuilder()
+    if not usuario:
+        message=rb.add_message("Usuario no encontrado").add_status_code(404).add_data({}).add_path(f"/api/v1/usuarios/{id}").build()
+        
+        return message_mapper.dump(message, many=False), 404
+    message=rb.add_message("Usuario actualizado en el sistema").add_status_code(200).add_data(usuario_mapper.dump(usuario)).add_path(f"/api/v1/usuarios/{id}").build()
+        
+    return message_mapper.dump(message, many=False), 200
+
+ 
